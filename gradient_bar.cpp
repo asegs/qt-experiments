036d3591cc8c7bf81505933b032973039087c224
Aaron <aarsegal@gmail.com>
2025-07-22 22:36:00 -0400

    Got cool gradient bar

--- gradient_bar.cpp
+++ gradient_bar.cpp
@@ -8,6 +8,9 @@ int HIGH_HUE = 0;
 int SATURATION = 128;
 int LIGHTNESS = 128;
 
+double FIRST_STOP = 0.6;
+double SECOND_STOP = 0.8;
+
 GradientBar::GradientBar(QWidget *parent): QProgressBar(parent) {}
 
 QString getHslForVal(int val) {
@@ -23,11 +26,30 @@ QString getHslForVal(int val) {
     return QString::fromStdString(style);
 }
 
+double min(double a, double b) {
+    if (a < b) {
+        return a;
+    }
+    return b;
+}
+
+QString getGradientSliderForVal(int val) {
+    double percent = ((double) val) / 100.0;
+    double firstStop = min(0.9999, FIRST_STOP/percent);
+    double secondStop = min(0.99999, SECOND_STOP/percent);
+    std::string style = std::format(
+            "QProgressBar::chunk {{background: qlineargradient(x0: 0, x2: 1, stop: 0 green, stop: {} green, stop: {} orange, stop: 1 red)}}",
+            firstStop,
+            secondStop
+                );
+    return QString::fromStdString(style);
+}
+
 
 void GradientBar::setValue(int val)
    {
     this->setStyleSheet(
-       getHslForVal(val)
+       getGradientSliderForVal(val)
     );
     QProgressBar::setValue(val);
    }
